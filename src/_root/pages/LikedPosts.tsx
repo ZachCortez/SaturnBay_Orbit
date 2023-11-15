5d40a2cb86cb53b210320aed2b82c79020d7d0bd
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2023-11-14 22:50:32 -0800

    Add files via upload

---
+++ src/_root/pages/LikedPosts.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const LikedPosts = () => {
+  return (
+    <div>LikedPosts</div>
+  )
+}
+
+export default LikedPosts
+
+// import { GridPostList, Loader } from "@/components/shared";
+// import { useGetCurrentUser } from "@/lib/react-query/queries";
+
+// const LikedPosts = () => {
+//   const { data: currentUser } = useGetCurrentUser();
+
+//   if (!currentUser)
+//     return (
+//       <div className="flex-center w-full h-full">
+//         <Loader />
+//       </div>
+//     );
+
+//   return (
+//     <>
+//       {currentUser.liked.length === 0 && (
+//         <p className="text-light-4">No liked posts</p>
+//       )}
+
+//       <GridPostList posts={currentUser.liked} showStats={false} />
+//     </>
+//   );
+// };
+
+// export default LikedPosts;
