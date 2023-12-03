ab7dbf56ec5228c273201917568a67d79153d74b
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2023-12-02 18:11:25 -0800

    Add files via upload

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -8,12 +8,10 @@ const RootLayout = () => {
   return (
     <div className="w-full md:flex">
       <Topbar />
-      <LeftSidebar />
-
-      <section className="flex flex-1 h-full">
-        <Outlet />
-      </section>
-
+        <LeftSidebar />
+          <section className="flex flex-1 h-full">
+            <Outlet />
+          </section>
       <Bottombar />
     </div>
   )
--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -4,16 +4,22 @@ import SearchResults from '@/components/shared/SearchResults';
 import { Input } from '@/components/ui/input'
 import useDebounce from '@/hooks/useDebounce';
 import { useGetPosts, useSearchPosts } from '@/lib/react-query/queriesAndMutations';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 
 const Explore = () => {
+  const { ref, inView } = useInView();
   const {data: posts, fetchNextPage, hasNextPage } = useGetPosts();
 
   const [searchValue, setSearchValue] = useState('')
   const debounceValue = useDebounce(searchValue, 500);
   const { data: searchedPosts, isFetching: isSearchFetching } =
   useSearchPosts(debounceValue)
-
+// ---Infinite Scrolling--- //
+  useEffect(() => {
+    if(inView && !searchValue) fetchNextPage();
+  },[inView, searchValue])
+// ------------------------//
   if(!posts) {
     return(
       <div className="flex-center w-full h-full">
@@ -74,6 +80,13 @@ const Explore = () => {
         <GridPostList key={`page-${index}`} posts={item.documents} />
       ))}
       </div>
+
+      {hasNextPage && !searchValue && (
+        <div ref={ref} className="mt-10">
+          <Loader />
+        </div>
+
+      )}
     </div>
   )
 }
