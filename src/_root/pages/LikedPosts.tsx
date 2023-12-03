c54ab3d754b3ef66ade9297c6abfb25988fb2c0b
ZachCortez <70867089+ZachCortez@users.noreply.github.com>
2023-12-03 14:15:47 -0800

    Add files via upload

--- src/_root/pages/LikedPosts.tsx
+++ src/_root/pages/LikedPosts.tsx
@@ -1,35 +1,27 @@
-import React from 'react'
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+
 
 const LikedPosts = () => {
+  const { data: currentUser } = useGetCurrentUser();
+
+  if (!currentUser)
+    return (
+      <div className="flex-center w-full h-full">
+        <Loader />
+      </div>
+    );
+
   return (
-    <div>LikedPosts</div>
-  )
-}
+    <>
+      {currentUser.liked.length === 0 && (
+        <p className="text-light-4">No liked posts</p>
+      )}
 
-export default LikedPosts
+      <GridPostList posts={currentUser.liked} showStats={false} />
+    </>
+  );
+};
 
-// import { GridPostList, Loader } from "@/components/shared";
-// import { useGetCurrentUser } from "@/lib/react-query/queries";
-
-// const LikedPosts = () => {
-//   const { data: currentUser } = useGetCurrentUser();
-
-//   if (!currentUser)
-//     return (
-//       <div className="flex-center w-full h-full">
-//         <Loader />
-//       </div>
-//     );
-
-//   return (
-//     <>
-//       {currentUser.liked.length === 0 && (
-//         <p className="text-light-4">No liked posts</p>
-//       )}
-
-//       <GridPostList posts={currentUser.liked} showStats={false} />
-//     </>
-//   );
-// };
-
-// export default LikedPosts;
+export default LikedPosts;
--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,20 +1,47 @@
+import GridPostList from "@/components/shared/GridPostList";
 import Loader from "@/components/shared/Loader";
 import PostStats from "@/components/shared/PostStats";
 import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/context/AuthContext";
-import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { useDeletePost, useGetPostById, useGetUserPosts } from "@/lib/react-query/queriesAndMutations"
 import { multiFormatDateString } from "@/lib/utils";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  const { data: post, isPending } = useGetPostById(id);
   const { user } = useUserContext();
 
-  const handleDeletePost = () => {}
+  const { data: post, isPending } = useGetPostById(id);
+  const { data: userPosts, isLoading: isUserPostLoading } = useGetUserPosts(
+    post?.creator.$id
+  );
+  const { mutate: deletePost } = useDeletePost();
+const relatedPosts = userPosts?.documents.filter(
+    (userPost) => userPost.$id !== id
+  );
+  const handleDeletePost = () => {deletePost({ postId: id, imageId: post?.imageId });
+  navigate(-1);
+};
+
 
   return (
     <div className="post_details-container">
+      <div className="hidden md:flex max-w-5xl w-full">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost">
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+      </div>
+
       {isPending ? <Loader/> : (
         <div className="post_details-card">
           <img
@@ -88,8 +115,21 @@ const PostDetails = () => {
             </div>
         </div>
       )}
+
+      <div className="w-full max-w-5xl">
+        <hr className="border w-full border-dark-4/80" />
+
+        <h3 className="body-bold md:h3-bold w-full my-10">
+          More Related Posts
+        </h3>
+        {isUserPostLoading || !relatedPosts ? (
+          <Loader />
+        ) : (
+          <GridPostList posts={relatedPosts} />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostDetails
